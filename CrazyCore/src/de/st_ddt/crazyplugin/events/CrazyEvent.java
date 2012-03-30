4542a04ac8569cbc9c9709764a55b11343f45e47
ProfCrazy <ST-DDT@gmx.de>
2012-03-30 15:18:31 +0200

    CrazyCore: added CrazyEvent (to add plugin events soon)

---
+++ CrazyCore/src/de/st_ddt/crazyplugin/events/CrazyEvent.java
@@ -0,0 +1,20 @@
+package de.st_ddt.crazyplugin.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class CrazyEvent extends Event
+{
+
+	private static final HandlerList handlers = new HandlerList();
+
+	public HandlerList getHandlers()
+	{
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+}
