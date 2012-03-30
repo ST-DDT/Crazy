7f3012ae0dd1fa5812eee6997d3757e0e3681d0a
ProfCrazy <ST-DDT@gmx.de>
2012-03-30 16:32:34 +0200

    CrazyCore: Fixed bug in ChatHelper

--- CrazyCore/src/de/st_ddt/crazyutil/ChatHelper.java
+++ CrazyCore/src/de/st_ddt/crazyutil/ChatHelper.java
@@ -1,9 +1,12 @@
 package de.st_ddt.crazyutil;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 
 import org.bukkit.ChatColor;
+
+import de.st_ddt.crazycore.CrazyCore;
 
 public class ChatHelper
 {
@@ -13,7 +16,7 @@ public class ChatHelper
 		return string.replaceAll("\\&0", ChatColor.BLACK.toString()).replaceAll("\\&1", ChatColor.DARK_BLUE.toString()).replaceAll("\\&2", ChatColor.DARK_GREEN.toString()).replaceAll("\\&3", ChatColor.DARK_AQUA.toString()).replaceAll("\\&4", ChatColor.DARK_RED.toString()).replaceAll("\\&5", ChatColor.DARK_PURPLE.toString()).replaceAll("\\&6", ChatColor.GOLD.toString()).replaceAll("\\&7", ChatColor.GRAY.toString()).replaceAll("\\&8", ChatColor.DARK_GRAY.toString()).replaceAll("\\&9", ChatColor.BLUE.toString()).replaceAll("\\&A", ChatColor.GREEN.toString()).replaceAll("\\&B", ChatColor.AQUA.toString()).replaceAll("\\&C", ChatColor.RED.toString()).replaceAll("\\&D", ChatColor.LIGHT_PURPLE.toString()).replaceAll("\\&E", ChatColor.YELLOW.toString()).replaceAll("\\&F", ChatColor.WHITE.toString()).replaceAll("\\&G", ChatColor.MAGIC.toString());
 	}
 
-	public static String putArgs(String text, String[] args)
+	public static String putArgs(String text, String... args)
 	{
 		String res = text;
 		int length = args.length;
@@ -43,7 +46,7 @@ public class ChatHelper
 		if (length == 0)
 			return "";
 		String res = strings[0];
-		for (int i = 0; i < length; i++)
+		for (int i = 1; i < length; i++)
 			res = res + seperator + strings[i];
 		return res;
 	}
@@ -63,5 +66,10 @@ public class ChatHelper
 		while (list.hasNext())
 			res = res + seperator + list.next();
 		return res;
+	}
+
+	public static String dateToString(Date date)
+	{
+		return CrazyCore.DateFormat.format(date);
 	}
 }
