f84dd848819b91179ceb926e5e801b3a675be9e4
ProfCrazy <ST-DDT@gmx.de>
2012-05-05 16:09:23 +0200

    CrazyCore: added comment how a constructor for config saveable classes may look like

--- CrazyCore/src/de/st_ddt/crazyutil/ConfigurationSaveable.java
+++ CrazyCore/src/de/st_ddt/crazyutil/ConfigurationSaveable.java
@@ -5,5 +5,6 @@ import org.bukkit.configuration.ConfigurationSection;
 public interface ConfigurationSaveable
 {
 
+	// public ConfigurationSaveable(ConfigurationSection config);
 	public void save(ConfigurationSection config, String path);
 }
