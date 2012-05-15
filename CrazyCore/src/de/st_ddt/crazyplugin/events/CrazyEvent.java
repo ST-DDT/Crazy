762f38c96d9869c91d59be0b6edfef6a8074a8ee
ProfCrazy <ST-DDT@gmx.de>
2012-05-15 10:06:23 +0200

    CrazyCore: code cleanup

--- CrazyCore/src/de/st_ddt/crazyplugin/events/CrazyEvent.java
+++ CrazyCore/src/de/st_ddt/crazyplugin/events/CrazyEvent.java
@@ -1,6 +1,7 @@
 package de.st_ddt.crazyplugin.events;
 
 import org.bukkit.event.Event;
+
 import de.st_ddt.crazyplugin.CrazyPlugin;
 
 public abstract class CrazyEvent<T extends CrazyPlugin> extends Event
@@ -8,7 +9,7 @@ public abstract class CrazyEvent<T extends CrazyPlugin> extends Event
 
 	protected final T plugin;
 
-	public CrazyEvent(T plugin)
+	public CrazyEvent(final T plugin)
 	{
 		super();
 		this.plugin = plugin;
