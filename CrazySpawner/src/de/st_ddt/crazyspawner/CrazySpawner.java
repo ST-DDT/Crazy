26feb56584d5d932cc004a7f596e9dcb582a117a
ST-DDT <ST-DDT@gmx.de>
2012-06-17 03:44:38 +0200

    CrazySpawner: added spawn repeations, delays and intervals

--- CrazySpawner/src/de/st_ddt/crazyspawner/CrazySpawner.java
+++ CrazySpawner/src/de/st_ddt/crazyspawner/CrazySpawner.java
@@ -1,5 +1,6 @@
 package de.st_ddt.crazyspawner;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Animals;
@@ -27,52 +28,8 @@ public class CrazySpawner extends CrazyPlugin
 		Player player = (Player) sender;
 		if (commandLabel.equalsIgnoreCase("cms") || commandLabel.equalsIgnoreCase("crazymobs") || commandLabel.equalsIgnoreCase("crazyspawn"))
 		{
-			if (!player.hasPermission("crazyspawner.spawn"))
-				throw new CrazyCommandPermissionException();
-			CreatureType type = null;
-			int amount = 1;
-			switch (args.length)
-			{
-				case 2:
-					try
-					{
-						amount = Integer.parseInt(args[1]);
-					}
-					catch (NumberFormatException e)
-					{
-						throw new CrazyCommandParameterException(2, "Integer/Number");
-					}
-					if (amount <= 0)
-						throw new CrazyCommandParameterException(2, "positive Numbers");
-				case 1:
-					try
-					{
-						type = CreatureType.valueOf(args[0].toUpperCase());// .fromName(args[0]);
-					}
-					catch (Exception e)
-					{
-						type = null;
-					}
-					if (type == null)
-						throw new CrazyCommandParameterException(1, "Monstername", "sheep", "creeper", "zombie");
-					try
-					{
-						Location location = player.getLocation();
-						while (location.add(0, -1, 0).getBlock().isEmpty() && location.getBlockZ() > 0)
-							;
-						location.add(0, 1, 0);
-						for (int i = 0; i < amount; i++)
-							location.getWorld().spawnCreature(location, type);
-					}
-					catch (Exception e)
-					{
-						throw new CrazyCommandException();
-					}
-					sendLocaleMessage("COMMAND.SPAWN", sender, String.valueOf(amount), type.getName());
-					return true;
-				default:
-					throw new CrazyCommandUsageException("/crazyspawn <Monstername> [Amount]");
-			}
+			commandSpawn(player, args);
+			return true;
 		}
 		if (commandLabel.equalsIgnoreCase("cmk") || commandLabel.equalsIgnoreCase("crazymobs") || commandLabel.equalsIgnoreCase("butcher") || commandLabel.equalsIgnoreCase("killall"))
 		{
@@ -118,6 +75,95 @@ public class CrazySpawner extends CrazyPlugin
 			}
 		}
 		return false;
+	}
+
+	private void commandSpawn(Player player, String[] args) throws CrazyCommandException
+	{
+		if (!player.hasPermission("crazyspawner.spawn"))
+			throw new CrazyCommandPermissionException();
+		if (args.length < 2)
+			throw new CrazyCommandUsageException("/cms <Monstername> [[amount:]Integer] [delay:Integer] [repeat:Integer] [interval:Integer]");
+		final int length = args.length;
+		CreatureType type = null;
+		int amount = 1;
+		int delay = 0;
+		int repeat = 0;
+		int interval = 1;
+		try
+		{
+			type = CreatureType.valueOf(args[0].toUpperCase());// .fromName(args[0]);
+		}
+		catch (Exception e)
+		{
+			type = null;
+		}
+		if (type == null)
+			throw new CrazyCommandParameterException(1, "Monstername", "sheep", "creeper", "zombie");
+		for (int i = 1; i < length; i++)
+		{
+			String arg = args[i];
+			if (arg.startsWith("amount:"))
+				try
+				{
+					amount = Integer.parseInt(arg.substring(7));
+					if (amount < 0)
+						throw new CrazyCommandParameterException(i, "positive Integer");
+				}
+				catch (final NumberFormatException e)
+				{
+					throw new CrazyCommandParameterException(i, "amount:Integer");
+				}
+			else if (arg.startsWith("delay:"))
+				try
+				{
+					delay = Integer.parseInt(arg.substring(6));
+					if (delay <= 0)
+						throw new CrazyCommandParameterException(i, "positive Integer");
+				}
+				catch (final NumberFormatException e)
+				{
+					throw new CrazyCommandParameterException(i, "delay:Integer");
+				}
+			else if (arg.startsWith("repeat:"))
+				try
+				{
+					repeat = Integer.parseInt(arg.substring(7));
+					if (repeat < 0)
+						throw new CrazyCommandParameterException(i, "positive Integer");
+				}
+				catch (final NumberFormatException e)
+				{
+					throw new CrazyCommandParameterException(i, "repeat:Integer");
+				}
+			else if (arg.startsWith("interval:"))
+				try
+				{
+					interval = Integer.parseInt(arg.substring(9));
+					if (interval <= 0)
+						throw new CrazyCommandParameterException(i, "positive Integer");
+				}
+				catch (final NumberFormatException e)
+				{
+					throw new CrazyCommandParameterException(i, "interval:Integer");
+				}
+		}
+		try
+		{
+			Location location = player.getLocation();
+			while (location.add(0, -1, 0).getBlock().isEmpty() && location.getBlockZ() > 0)
+				;
+			location.add(0, 1, 0);
+			delay -= interval;
+			delay *= 20;
+			interval *= 20;
+			for (int i = 0; i <= repeat; i++)
+				Bukkit.getScheduler().scheduleAsyncDelayedTask(this, new CrazySpawnerSpawnTask(location, type, amount), delay += interval);
+		}
+		catch (Exception e)
+		{
+			throw new CrazyCommandException();
+		}
+		sendLocaleMessage("COMMAND.SPAWN", player, String.valueOf(amount), type.getName());
 	}
 
 	@Override
---
+++ CrazySpawner/src/de/st_ddt/crazyspawner/CrazySpawnerSpawnTask.java
@@ -0,0 +1,29 @@
+package de.st_ddt.crazyspawner;
+
+import org.bukkit.Location;
+import org.bukkit.entity.CreatureType;
+
+@SuppressWarnings("deprecation")
+public class CrazySpawnerSpawnTask implements Runnable
+{
+
+	protected Location location;
+	protected CreatureType creature;
+	protected int amount;
+
+	public CrazySpawnerSpawnTask(Location location, CreatureType creature, int amount)
+	{
+		super();
+		this.location = location;
+		this.creature = creature;
+		this.amount = amount;
+	}
+
+	@Override
+	public void run()
+	{
+		System.out.println("Test");
+		for (int i = 0; i < amount; i++)
+			location.getWorld().spawnCreature(location, creature);
+	}
+}
