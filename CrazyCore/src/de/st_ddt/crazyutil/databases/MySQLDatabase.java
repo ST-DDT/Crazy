dd8bff03f52247bd04413862fdadfabeaf57da17
ST-DDT <ST-DDT@gmx.de>
2012-07-12 19:25:00 +0200

    CrazyCore: fixed missing method call for mysql database access

--- CrazyCore/src/de/st_ddt/crazyutil/databases/MySQLDatabase.java
+++ CrazyCore/src/de/st_ddt/crazyutil/databases/MySQLDatabase.java
@@ -141,6 +141,7 @@ public class MySQLDatabase<S extends MySQLDatabaseEntry> extends BasicDatabase<S
 		{
 			query = connection.getConnection().createStatement();
 			result = query.executeQuery("SELECT * FROM `" + table + "` WHERE " + primary.getName() + "='" + key + "' LIMIT 1");
+			result.next();
 			try
 			{
 				res = constructor.newInstance(result, columnNames);
