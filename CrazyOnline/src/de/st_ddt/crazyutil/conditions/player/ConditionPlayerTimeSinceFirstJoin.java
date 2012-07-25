f17b5cfff2f16d440d5239e1a9e15d3686845513
ST-DDT <ST-DDT@gmx.de>
2012-07-25 08:53:33 +0200

    CrazyOnline: added ConditionPlayerTimeSinceFirstJoin,LastJoin,LastLogout

---
+++ CrazyOnline/src/de/st_ddt/crazyutil/conditions/player/ConditionPlayerTimeSinceFirstJoin.java
@@ -0,0 +1,45 @@
+package de.st_ddt.crazyutil.conditions.player;
+
+import java.util.Date;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import de.st_ddt.crazyonline.CrazyOnline;
+
+public class ConditionPlayerTimeSinceFirstJoin extends ConditionPlayer
+{
+
+	protected long time;
+	protected boolean above;
+
+	public ConditionPlayerTimeSinceFirstJoin(final ConfigurationSection config)
+	{
+		super(config);
+		time = config.getLong("time", -1);
+		above = config.getBoolean("above", true);
+	}
+
+	@Override
+	public void save(final ConfigurationSection config, final String path)
+	{
+		super.save(config, path);
+		config.set(path + "time", time);
+		config.set(path + "above", above);
+	}
+
+	@Override
+	public String getTypeIdentifier()
+	{
+		return "TimeSinceFirstJoin";
+	}
+
+	@Override
+	public boolean match(final Player tester)
+	{
+		if (above)
+			return CrazyOnline.getPlugin().getPlayerData(tester).getFirstLogin().getTime() - new Date().getTime() >= time;
+		else
+			return CrazyOnline.getPlugin().getPlayerData(tester).getFirstLogin().getTime() - new Date().getTime() < time;
+	}
+}
