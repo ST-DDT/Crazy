b8009a0d69c8bb706e706053d5c1864912e7e6a8
ST-DDT <ST-DDT@gmx.de>
2012-08-08 20:12:55 +0200

    CrazyOnline: fixed error in mysqldatabase

--- CrazyOnline/src/de/st_ddt/crazyonline/databases/CrazyOnlineMySQLDatabase.java
+++ CrazyOnline/src/de/st_ddt/crazyonline/databases/CrazyOnlineMySQLDatabase.java
@@ -14,21 +14,21 @@ import de.st_ddt.crazyutil.databases.PlayerDataDatabase;
 public class CrazyOnlineMySQLDatabase extends MySQLDatabase<OnlinePlayerData> implements PlayerDataDatabase<OnlinePlayerData>
 {
 
-	public CrazyOnlineMySQLDatabase(final String tableName, final ConfigurationSection config)
+	public CrazyOnlineMySQLDatabase(final String tableName, final ConfigurationSection config) throws Exception
 	{
 		super(OnlinePlayerData.class, tableName, config, getColumns(config), 0);
 	}
 
 	private static MySQLColumn[] getColumns(final ConfigurationSection config)
 	{
-		final MySQLColumn[] columns = new MySQLColumn[5];
+		final MySQLColumn[] columns = new MySQLColumn[6];
 		final String defaultDate = "'" + CrazyPluginInterface.DateFormat.format(new Date(0)) + "'";
-		columns[0] = new MySQLColumn(config.getString("column.name", "name"), "CHAR(50)", true, false);
-		columns[1] = new MySQLColumn(config.getString("column.firstLogin", "firstLogin"), "CHAR(19)", defaultDate, false, false);
-		columns[2] = new MySQLColumn(config.getString("column.lastLogin", "lastLogin"), "CHAR(19)", defaultDate, false, false);
-		columns[3] = new MySQLColumn(config.getString("column.lastLogout", "lastLogout"), "CHAR(19)", defaultDate, false, false);
-		columns[4] = new MySQLColumn(config.getString("column.onlineTime", "onlineTime"), "INT", "0", false, false);
-		columns[0] = new MySQLColumn(config.getString("column.ip", "ip"), "CHAR(50)", true, false);
+		columns[0] = new MySQLColumn("name", "CHAR(50)", true, false);
+		columns[1] = new MySQLColumn("firstLogin", "CHAR(19)", defaultDate, false, false);
+		columns[2] = new MySQLColumn("lastLogin", "CHAR(19)", defaultDate, false, false);
+		columns[3] = new MySQLColumn("lastLogout", "CHAR(19)", defaultDate, false, false);
+		columns[4] = new MySQLColumn("onlineTime", "INT", "0", false, false);
+		columns[5] = new MySQLColumn("ip", "CHAR(50)", true, false);
 		return columns;
 	}
 
