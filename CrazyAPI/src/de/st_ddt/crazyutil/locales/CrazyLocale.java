e1c377594224115ebc9aa9203d01d34db26ef023
ST-DDT <ST-DDT@gmx.de>
2012-08-08 13:51:18 +0200

    CrazyAPI: added CRAZYPLUGIN.DATABASE.ACCESSWARN to default messages

--- CrazyAPI/src/de/st_ddt/crazyutil/locales/CrazyLocale.java
+++ CrazyAPI/src/de/st_ddt/crazyutil/locales/CrazyLocale.java
@@ -52,6 +52,7 @@ public class CrazyLocale extends HashMap<String, CrazyLocale>
 		getLocaleHead().addLanguageEntry("en_en", "CRAZYPLUGIN.LANGUAGE.ERROR.AVAILABLE", "$0$ language files not available!");
 		getLocaleHead().addLanguageEntry("en_en", "CRAZYPLUGIN.LANGUAGE.LOADED", "Language $0$ loaded sucessfully!");
 		getLocaleHead().addLanguageEntry("en_en", "CRAZYPLUGIN.DATABASE.LOADED", "Loaded $0$ entries from database!");
+		getLocaleHead().addLanguageEntry("en_en", "CRAZYPLUGIN.DATABASE.ACCESSWARN", "&CWARNING! Cannot access Database!");
 		getLocaleHead().addLanguageEntry("en_en", "LANGUAGE.NAME", "English");
 	}
 
