4e77dc60de297303772d938297b96981d9f6d120
ST-DDT <ST-DDT@gmx.de>
2012-08-25 16:13:12 +0200

    CrazyAPI: removed invalid superinterface from Commandable

--- CrazyAPI/src/de/st_ddt/crazyutil/Commandable.java
+++ CrazyAPI/src/de/st_ddt/crazyutil/Commandable.java
@@ -4,7 +4,7 @@ import org.bukkit.command.CommandSender;
 
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
 
-public interface Commandable extends PlayerCommandable
+public interface Commandable
 {
 
 	public boolean command(CommandSender sender, String commandLabel, String[] args) throws CrazyException;
