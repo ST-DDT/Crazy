bc2f909679a0d73e7329451219b201019e9931bd
ST-DDT <ST-DDT@gmx.de>
2012-09-09 00:43:44 +0200

    CrazyArena: improved ArenaStatuses, added descriptions

--- CrazyArena/src/de/st_ddt/crazyarena/arenas/ArenaStatus.java
+++ CrazyArena/src/de/st_ddt/crazyarena/arenas/ArenaStatus.java
@@ -2,22 +2,60 @@ package de.st_ddt.crazyarena.arenas;
 
 public enum ArenaStatus
 {
-	INITIALIZING(false, false, false), CONSTRUCTING(false, false, false), READY(false, true, false), SELECTING(true, true, true), WAITING(true, false, true), PLAYING(true, false, true), PAUSED(true, false, true), FINISHED(false, false, false), SHUTDOWN(false, false, false), DISABLED(false, false, false), ERROR(false, false, false);
+	// Loading
+	INITIALIZING(),
+	// Edit/Setup Arenas
+	CONSTRUCTING(),
+	// Enabled Status START
+	// Waiting for first player to join
+	READY(false, true, false),
+	// Players select their equip/team (waiting for players become ready)
+	SELECTING(true, true, true),
+	// Waiting for arena to start (show rules/teams)
+	WAITING(true, false, true),
+	// Arena in progress
+	PLAYING(true, false, true),
+	// Arena currently paused
+	PAUSED(true, false, true),
+	// Arena finished, give rewards, restore inventories, restore terrain
+	FINISHED(false, false, false),
+	// Enabled Status END
+	// Disabled
+	DISABLED(),
+	// Arena is going to be deleted/server shuting down
+	SHUTDOWN(),
+	// Arena is in Error Mode and cannot continue operating
+	ERROR();
 
+	private final boolean enabled;
+	private final boolean active;
 	private final boolean spectator;
 	private final boolean join;
-	private final boolean active;
+
+	private ArenaStatus()
+	{
+		this.enabled = false;
+		this.spectator = false;
+		this.join = false;
+		this.active = false;
+	}
 
 	private ArenaStatus(boolean spectator, boolean join, boolean active)
 	{
+		this.enabled = true;
 		this.spectator = spectator;
 		this.join = join;
 		this.active = active;
 	}
 
-	public boolean allowSpectators()
+	public boolean isEnabled()
 	{
-		return spectator;
+		return enabled;
+	}
+
+	public boolean isActive()
+	{
+		return active;
 	}
 
 	public boolean allowJoins()
@@ -25,8 +63,8 @@ public enum ArenaStatus
 		return join;
 	}
 
-	public boolean isActive()
+	public boolean allowSpectators()
 	{
-		return active;
+		return spectator;
 	}
 }
