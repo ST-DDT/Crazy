262e3e0b28cae1b47904b7ea83905e2231143f40
ST-DDT <ST-DDT@gmx.de>
2012-09-09 00:55:08 +0200

    CrazyArenaRace: added missing registration of race arena type

--- CrazyArenaRace/src/de/st_ddt/crazyarena/CrazyArenaRace.java
+++ CrazyArenaRace/src/de/st_ddt/crazyarena/CrazyArenaRace.java
@@ -1,12 +1,14 @@
 package de.st_ddt.crazyarena;
 
 import de.st_ddt.crazyarena.CrazyArenaPlugin;
+import de.st_ddt.crazyarena.arenas.race.RaceArena;
 
 public class CrazyArenaRace extends CrazyArenaPlugin
 {
 
 	protected void registerArenaTypes()
 	{
+		registerArenaType("Race", RaceArena.class);
 	}
 
 	public String[] getArenaTypes()
