419f547380b69eef5fd0d20edb867e7802a46565
ST-DDT <ST-DDT@gmx.de>
2012-10-16 13:57:31 +0200

    CrazyLogin: added new interface for encryptors that update the users passwords

---
+++ CrazyAPI_Login/src/de/st_ddt/crazylogin/crypt/UpdatingEncryptor.java
@@ -0,0 +1,18 @@
+package de.st_ddt.crazylogin.crypt;
+
+public interface UpdatingEncryptor extends Encryptor
+{
+	
+	/**
+	 * Encrypts the password. (Also used when using a new encryption algorithm)
+	 * 
+	 * @param name
+	 *            The name this password belongs too.
+	 * @param salt
+	 *            The salt used to increase security. (Default length=10)
+	 * @param password
+	 *            The password that should be encrypted.
+	 * @return The encrypted password
+	 */
+	public String encrypt(String name, String salt, String password);
+}
