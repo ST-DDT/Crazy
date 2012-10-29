a4959cded3e6180aa0ef319c14337e740338a401
ST-DDT <ST-DDT@gmx.de>
2012-10-29 15:19:51 +0100

    CrazyCore: enabled tab completion support, removed support for Minecraft 1.2.5-1.3.2R2

--- CrazyAPI/src/de/st_ddt/crazyplugin/commands/CrazyCommandExecutorInterface.java
+++ CrazyAPI/src/de/st_ddt/crazyplugin/commands/CrazyCommandExecutorInterface.java
@@ -3,17 +3,18 @@ package de.st_ddt.crazyplugin.commands;
 import java.util.List;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
 
-public interface CrazyCommandExecutorInterface extends CommandExecutor
+public interface CrazyCommandExecutorInterface extends TabExecutor
 {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args);
 
+	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args);
 
 	public void command(CommandSender sender, String[] args) throws CrazyException;
