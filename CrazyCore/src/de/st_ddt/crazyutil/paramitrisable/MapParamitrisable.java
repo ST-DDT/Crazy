a3f041b3066c0009b6f94669fbc1b613598a502c
ST-DDT <ST-DDT@gmx.de>
2012-11-01 21:46:20 +0100

    CrazyCore: added MapParamitrisable

---
+++ CrazyCore/src/de/st_ddt/crazyutil/paramitrisable/MapParamitrisable.java
@@ -0,0 +1,41 @@
+package de.st_ddt.crazyutil.paramitrisable;
+
+import java.util.Map;
+
+import de.st_ddt.crazyplugin.exceptions.CrazyCommandNoSuchException;
+import de.st_ddt.crazyplugin.exceptions.CrazyException;
+import de.st_ddt.crazyutil.paramitrisable.Paramitrisable.TypedParamitrisable;
+
+public class MapParamitrisable<S> extends TypedParamitrisable<S>
+{
+
+	private final String type;
+	private final Map<String, S> values;
+	private final boolean lowercase;
+
+	public MapParamitrisable(String type, Map<String, S> values, S defaultValue)
+	{
+		super(defaultValue);
+		this.type = type;
+		this.values = values;
+		this.lowercase = false;
+	}
+
+	public MapParamitrisable(String type, Map<String, S> values, S defaultValue, boolean lowercase)
+	{
+		super(defaultValue);
+		this.type = type;
+		this.values = values;
+		this.lowercase = lowercase;
+	}
+
+	@Override
+	public void setParameter(String parameter) throws CrazyException
+	{
+		if (lowercase)
+			parameter = parameter.toLowerCase();
+		if (!values.containsKey(parameter))
+			throw new CrazyCommandNoSuchException(type, parameter, values.keySet());
+		value = values.get(parameter);
+	}
+}
