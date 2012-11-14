41b85a26fbe0d58d085c7367ca5e2e82767e532d
ST-DDT <ST-DDT@gmx.de>
2012-11-14 12:40:52 +0100

    CrazyEnchanter: add TabSupport, use Permission Module

--- CrazyEnchanter/src/de/st_ddt/crazyenchanter/commands/CrazyEnchanterCommandEnchant.java
+++ CrazyEnchanter/src/de/st_ddt/crazyenchanter/commands/CrazyEnchanterCommandEnchant.java
@@ -1,5 +1,8 @@
 package de.st_ddt.crazyenchanter.commands;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -9,11 +12,11 @@ import de.st_ddt.crazyenchanter.CrazyEnchanter;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandCircumstanceException;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandErrorException;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandParameterException;
-import de.st_ddt.crazyplugin.exceptions.CrazyCommandPermissionException;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandUsageException;
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
 import de.st_ddt.crazyutil.ChatHelper;
 import de.st_ddt.crazyutil.locales.Localized;
+import de.st_ddt.crazyutil.modules.permissions.PermissionModule;
 
 public class CrazyEnchanterCommandEnchant extends CrazyEnchanterCommandExecutor
 {
@@ -30,8 +33,6 @@ public class CrazyEnchanterCommandEnchant extends CrazyEnchanterCommandExecutor
 		if (!(sender instanceof Player))
 			return;
 		final Player player = (Player) sender;
-		if (!player.hasPermission("crazyenchanter.enchant"))
-			throw new CrazyCommandPermissionException();
 		final ItemStack item = player.getItemInHand();
 		if (item == null)
 			throw new CrazyCommandCircumstanceException("when helding an enchantable item in hand!");
@@ -76,5 +77,24 @@ public class CrazyEnchanterCommandEnchant extends CrazyEnchanterCommandExecutor
 				throw new CrazyCommandErrorException(e);
 			}
 		plugin.sendLocaleMessage("COMMAND.ENCHANT.SUCCESS", sender, item.getType().toString(), enchantment.getName(), level);
+	}
+
+	@Override
+	public List<String> tab(final CommandSender sender, final String[] args)
+	{
+		if (args.length != 1)
+			return null;
+		final List<String> res = new LinkedList<String>();
+		final String arg = args[0].toLowerCase();
+		for (final Enchantment enchantment : Enchantment.values())
+			if (enchantment.getName().toLowerCase().startsWith(arg))
+				res.add(enchantment.getName());
+		return res;
+	}
+
+	@Override
+	public boolean hasAccessPermission(final CommandSender sender)
+	{
+		return PermissionModule.hasPermission(sender, "crazyenchanter.enchant");
 	}
 }
