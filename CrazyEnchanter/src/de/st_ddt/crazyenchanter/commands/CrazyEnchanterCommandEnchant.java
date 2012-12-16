05a76f5558c41fe3cc44bd1f73031cd2d5a510eb
ST-DDT <ST-DDT@gmx.de>
2012-12-16 16:00:37 +0100

    CrazyEnchanter: show only applicable enchantments in enchant command

--- CrazyEnchanter/src/de/st_ddt/crazyenchanter/commands/CrazyEnchanterCommandEnchant.java
+++ CrazyEnchanter/src/de/st_ddt/crazyenchanter/commands/CrazyEnchanterCommandEnchant.java
@@ -1,7 +1,10 @@
 package de.st_ddt.crazyenchanter.commands;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
@@ -11,12 +14,15 @@ import org.bukkit.inventory.ItemStack;
 import de.st_ddt.crazyenchanter.CrazyEnchanter;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandCircumstanceException;
 import de.st_ddt.crazyplugin.exceptions.CrazyCommandErrorException;
-import de.st_ddt.crazyplugin.exceptions.CrazyCommandParameterException;
-import de.st_ddt.crazyplugin.exceptions.CrazyCommandUsageException;
+import de.st_ddt.crazyplugin.exceptions.CrazyCommandExecutorException;
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
-import de.st_ddt.crazyutil.ChatHelper;
+import de.st_ddt.crazyutil.ChatHelperExtended;
 import de.st_ddt.crazyutil.locales.Localized;
 import de.st_ddt.crazyutil.modules.permissions.PermissionModule;
+import de.st_ddt.crazyutil.paramitrisable.BooleanParamitrisable;
+import de.st_ddt.crazyutil.paramitrisable.IntegerParamitrisable;
+import de.st_ddt.crazyutil.paramitrisable.MapParamitrisable;
+import de.st_ddt.crazyutil.paramitrisable.Paramitrisable;
 
 public class CrazyEnchanterCommandEnchant extends CrazyEnchanterCommandExecutor
 {
@@ -31,52 +37,41 @@ public class CrazyEnchanterCommandEnchant extends CrazyEnchanterCommandExecutor
 	public void command(final CommandSender sender, final String[] args) throws CrazyException
 	{
 		if (!(sender instanceof Player))
-			return;
+			throw new CrazyCommandExecutorException(false);
 		final Player player = (Player) sender;
 		final ItemStack item = player.getItemInHand();
 		if (item == null)
 			throw new CrazyCommandCircumstanceException("when helding an enchantable item in hand!");
 		if (item.getMaxStackSize() > 1)
 			throw new CrazyCommandCircumstanceException("when helding an enchantable item in hand!");
-		Enchantment enchantment = null;
-		int level = 1;
-		boolean unsafe = false;
-		switch (args.length)
-		{
-			case 3:
-				if (args[2].equals("1") || args[2].equalsIgnoreCase("true"))
-					unsafe = true;
-			case 2:
-				try
-				{
-					level = Integer.parseInt(args[1]);
-				}
-				catch (final NumberFormatException e)
-				{
-					throw new CrazyCommandParameterException(2, "Integer");
-				}
-				if (level < 0)
-					throw new CrazyCommandParameterException(2, "positive Integer");
-			case 1:
-				enchantment = Enchantment.getByName(args[0]);
-				if (enchantment == null)
-					throw new CrazyCommandParameterException(1, "Enchantment", ChatHelper.listingString(plugin.getEnchantmentNames()));
-				break;
-			default:
-				throw new CrazyCommandUsageException("<Enchantment> [Level [Unsafe]]");
-		}
-		if (unsafe)
-			item.addUnsafeEnchantment(enchantment, level);
+		final Map<String, Enchantment> applicableEnchantments = new TreeMap<String, Enchantment>();
+		for (final Enchantment enchantment : Enchantment.values())
+			if (enchantment.canEnchantItem(item))
+				applicableEnchantments.put(enchantment.getName(), enchantment);
+		final Map<String, Paramitrisable> params = new HashMap<String, Paramitrisable>();
+		final MapParamitrisable<Enchantment> enchantment = new MapParamitrisable<Enchantment>("Enchantment", applicableEnchantments, null);
+		params.put("e", enchantment);
+		params.put("enchantment", enchantment);
+		params.put("enchantments", enchantment);
+		final IntegerParamitrisable level = new IntegerParamitrisable(1);
+		params.put("l", level);
+		params.put("level", level);
+		final BooleanParamitrisable unsafe = new BooleanParamitrisable(false);
+		params.put("u", unsafe);
+		params.put("unsafe", unsafe);
+		ChatHelperExtended.readParameters(args, params);
+		if (unsafe.getValue())
+			item.addUnsafeEnchantment(enchantment.getValue(), level.getValue());
 		else
 			try
 			{
-				item.addEnchantment(enchantment, level);
+				item.addEnchantment(enchantment.getValue(), level.getValue());
 			}
 			catch (final IllegalArgumentException e)
 			{
 				throw new CrazyCommandErrorException(e);
 			}
-		plugin.sendLocaleMessage("COMMAND.ENCHANT.SUCCESS", sender, item.getType().toString(), enchantment.getName(), level);
+		plugin.sendLocaleMessage("COMMAND.ENCHANT.SUCCESS", sender, item.getType().toString(), enchantment.getValue().getName(), level.getValue());
 	}
 
 	@Override
@@ -84,11 +79,16 @@ public class CrazyEnchanterCommandEnchant extends CrazyEnchanterCommandExecutor
 	{
 		if (args.length != 1)
 			return null;
+		if (!(sender instanceof Player))
+			return null;
+		final Player player = (Player) sender;
+		final ItemStack item = player.getItemInHand();
 		final List<String> res = new LinkedList<String>();
 		final String arg = args[0].toLowerCase();
 		for (final Enchantment enchantment : Enchantment.values())
-			if (enchantment.getName().toLowerCase().startsWith(arg))
-				res.add(enchantment.getName());
+			if (enchantment.canEnchantItem(item))
+				if (enchantment.getName().toLowerCase().startsWith(arg))
+					res.add(enchantment.getName());
 		return res;
 	}
 
