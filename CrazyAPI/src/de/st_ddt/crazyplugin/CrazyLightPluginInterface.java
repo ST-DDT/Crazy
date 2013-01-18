3b0c74233cf78856fd7076c30aa4c0e24eff3483
ST-DDT <ST-DDT@gmx.de>
2013-01-18 03:47:19 +0100

    CrazyAPI: made Plugins Comparable

--- CrazyAPI/src/de/st_ddt/crazyplugin/CrazyLightPluginInterface.java
+++ CrazyAPI/src/de/st_ddt/crazyplugin/CrazyLightPluginInterface.java
@@ -11,7 +11,7 @@ import de.st_ddt.crazyplugin.data.ParameterData;
 import de.st_ddt.crazyutil.ChatHeaderProvider;
 import de.st_ddt.crazyutil.Named;
 
-public interface CrazyLightPluginInterface extends Named, ChatHeaderProvider, ParameterData, Plugin
+public interface CrazyLightPluginInterface extends Named, ChatHeaderProvider, ParameterData, Plugin, Comparable<CrazyLightPluginInterface>
 {
 
 	public static DateFormat DATETIMEFORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
