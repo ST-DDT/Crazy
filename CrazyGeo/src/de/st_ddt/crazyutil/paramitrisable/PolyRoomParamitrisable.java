7c800dcadcf9ed63ff8a26a656325e6a4c6a8251
ST-DDT <ST-DDT@gmx.de>
2013-01-27 03:21:21 +0100

    CrazyGeo: added basic geo paramitrisables (Bleeding)

---
+++ CrazyGeo/src/de/st_ddt/crazyutil/paramitrisable/PolyRoomParamitrisable.java
@@ -0,0 +1,19 @@
+package de.st_ddt.crazyutil.paramitrisable;
+
+import de.st_ddt.crazyplugin.exceptions.CrazyException;
+import de.st_ddt.crazyutil.poly.room.Room;
+
+public class PolyRoomParamitrisable extends TypedParamitrisable<Room>
+{
+
+	public PolyRoomParamitrisable(final Room defaultValue)
+	{
+		super(defaultValue);
+	}
+
+	@Override
+	public void setParameter(final String parameter) throws CrazyException
+	{
+		// EDIT Automatisch generierter Methodenstub
+	}
+}
