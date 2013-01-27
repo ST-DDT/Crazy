8e4e4edb995dba0bb081a1076b9d1a641774524b
ST-DDT <ST-DDT@gmx.de>
2013-01-27 19:43:11 +0100

    CrazyArenas: improved default command executors

--- CrazyArena/src/de/st_ddt/crazyarena/command/ArenaCommandExecutor.java
+++ CrazyArena/src/de/st_ddt/crazyarena/command/ArenaCommandExecutor.java
@@ -2,12 +2,13 @@ package de.st_ddt.crazyarena.command;
 
 import de.st_ddt.crazyarena.CrazyArena;
 import de.st_ddt.crazyarena.CrazyArenaPlugin;
+import de.st_ddt.crazyarena.arenas.Arena;
 import de.st_ddt.crazyplugin.commands.CrazyCommandExecutor;
 
-public abstract class ArenaPluginCommandExecutor<S extends CrazyArenaPlugin> extends CrazyCommandExecutor<S>
+public abstract class ArenaCommandExecutor<S extends Arena<?>> extends CrazyCommandExecutor<S>
 {
 
-	public ArenaPluginCommandExecutor(final S plugin)
+	public ArenaCommandExecutor(final S plugin)
 	{
 		super(plugin);
 	}
@@ -16,4 +17,6 @@ public abstract class ArenaPluginCommandExecutor<S extends CrazyArenaPlugin> ext
 	{
 		return CrazyArena.getPlugin();
 	}
+	
+	public abstract CrazyArenaPlugin getArenaPlugin();
 }
--- CrazyArena/src/de/st_ddt/crazyarena/command/ArenaPlayerCommandExecutor.java
+++ CrazyArena/src/de/st_ddt/crazyarena/command/ArenaPlayerCommandExecutor.java
@@ -1,21 +1,58 @@
 package de.st_ddt.crazyarena.command;
 
-import de.st_ddt.crazyarena.CrazyArena;
-import de.st_ddt.crazyarena.arenas.Arena;
+import java.util.List;
 
-public abstract class ArenaPlayerCommandExecutor<S extends Arena<?>> extends PlayerCommandExecutor
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.st_ddt.crazyarena.arenas.Arena;
+import de.st_ddt.crazyplugin.exceptions.CrazyCommandExecutorException;
+import de.st_ddt.crazyplugin.exceptions.CrazyException;
+
+public abstract class ArenaPlayerCommandExecutor<S extends Arena<?>> extends ArenaCommandExecutor<S>
 {
 
-	protected final S arena;
-
-	public ArenaPlayerCommandExecutor(final CrazyArena plugin, final S arena)
+	public ArenaPlayerCommandExecutor(final S arena)
 	{
-		super(plugin);
-		this.arena = arena;
+		super(arena);
 	}
 
-	public S getArena()
+	@Override
+	public final void command(final CommandSender sender, final String[] args) throws CrazyException
 	{
-		return arena;
+		if (sender instanceof Player)
+			command((Player) sender, args);
+		else
+			throw new CrazyCommandExecutorException(false);
+	}
+
+	public abstract void command(Player player, String[] args) throws CrazyException;
+
+	@Override
+	public final List<String> tab(final CommandSender sender, final String[] args)
+	{
+		if (sender instanceof Player)
+			return tab((Player) sender, args);
+		else
+			return null;
+	}
+
+	public List<String> tab(final Player player, final String[] args)
+	{
+		return null;
+	}
+
+	@Override
+	public final boolean hasAccessPermission(final CommandSender sender)
+	{
+		if (sender instanceof Player)
+			return hasAccessPermission((Player) sender);
+		else
+			return true;
+	}
+
+	public boolean hasAccessPermission(final Player player)
+	{
+		return true;
 	}
 }
