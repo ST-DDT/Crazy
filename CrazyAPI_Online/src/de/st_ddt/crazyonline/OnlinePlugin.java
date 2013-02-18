fbe0c411520837d052e0cc242551e3bbddbac920
ST-DDT <ST-DDT@gmx.de>
2013-02-18 23:05:00 +0100

    CrazyOnline: some code cleanup

--- CrazyAPI_Online/src/de/st_ddt/crazyonline/OnlinePlugin.java
+++ CrazyAPI_Online/src/de/st_ddt/crazyonline/OnlinePlugin.java
@@ -1,18 +1,35 @@
 package de.st_ddt.crazyonline;
 
-import java.util.Set;
-
 import de.st_ddt.crazyonline.data.OnlineData;
 import de.st_ddt.crazyplugin.CrazyPlayerDataPluginInterface;
 
 public interface OnlinePlugin<S extends OnlineData> extends CrazyPlayerDataPluginInterface<OnlineData, S>
 {
 
-	static final OnlinePluginProvider ONLINEPLUGINPROVIDER = new OnlinePluginProvider();
-
-	public Set<S> getPlayerDatasPerIP(String IP);
+	public static final OnlinePluginProvider ONLINEPLUGINPROVIDER = new OnlinePluginProvider();
 
 	public int getAutoDelete();
 
 	public int dropInactiveAccounts();
+
+	public class OnlinePluginProvider
+	{
+
+		private OnlinePlugin<? extends OnlineData> plugin;
+
+		private OnlinePluginProvider()
+		{
+			super();
+		}
+
+		public OnlinePlugin<? extends OnlineData> getPlugin()
+		{
+			return plugin;
+		}
+
+		protected void setPlugin(final OnlinePlugin<? extends OnlineData> plugin)
+		{
+			this.plugin = plugin;
+		}
+	}
 }
