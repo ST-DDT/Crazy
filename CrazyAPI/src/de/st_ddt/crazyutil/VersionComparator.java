57ec406fc22f83a51f500c0f7efeab5dcc2acdfb
ST-DDT <ST-DDT@gmx.de>
2013-03-14 23:04:35 +0100

    CrazyCore: always return a version number in VersionComparator

--- CrazyAPI/src/de/st_ddt/crazyutil/VersionComparator.java
+++ CrazyAPI/src/de/st_ddt/crazyutil/VersionComparator.java
@@ -23,11 +23,11 @@ public class VersionComparator implements Comparator<String>
 		final Integer[] v1 = new Integer[length];
 		final Integer[] v2 = new Integer[length];
 		for (int i = 0; i < length1; i++)
-			v1[i] = Integer.parseInt(split1[i]);
+			v1[i] = cleanVersion(split1[i]);
 		for (int i = length1; i < length; i++)
 			v1[i] = 0;
 		for (int i = 0; i < length2; i++)
-			v2[i] = Integer.parseInt(split2[i]);
+			v2[i] = cleanVersion(split2[i]);
 		for (int i = length2; i < length; i++)
 			v2[i] = 0;
 		int i = 0;
@@ -43,5 +43,57 @@ public class VersionComparator implements Comparator<String>
 	public static int compareVersions(final CrazyPluginInterface plugin, final String version)
 	{
 		return compareVersions(plugin.getVersion(), version);
+	}
+
+	private static int cleanVersion(final String string)
+	{
+		try
+		{
+			return Integer.valueOf(string);
+		}
+		catch (final NumberFormatException e)
+		{
+			int version = 0;
+			for (final char cha : string.toCharArray())
+			{
+				version *= 10;
+				switch (cha)
+				{
+					case 0:
+						version += 0;
+						break;
+					case 1:
+						version += 1;
+						break;
+					case 2:
+						version += 2;
+						break;
+					case 3:
+						version += 3;
+						break;
+					case 4:
+						version += 4;
+						break;
+					case 5:
+						version += 5;
+						break;
+					case 6:
+						version += 6;
+						break;
+					case 7:
+						version += 7;
+						break;
+					case 8:
+						version += 8;
+						break;
+					case 9:
+						version += 9;
+						break;
+					default:
+						return version;
+				}
+			}
+			return version;
+		}
 	}
 }
