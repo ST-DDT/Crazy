645d3bf79bbf90aa924357d62dc417a0ccb9fe11
ST-DDT <ST-DDT@gmx.de>
2013-05-17 22:50:32 +0200

    CrazyLoginFilter: updated to latest CrazyCore version

--- CrazyLoginFilter/src/de/st_ddt/crazyloginfilter/commands/CrazyLoginFilterCommandExecutor.java
+++ CrazyLoginFilter/src/de/st_ddt/crazyloginfilter/commands/CrazyLoginFilterCommandExecutor.java
@@ -2,9 +2,9 @@ package de.st_ddt.crazyloginfilter.commands;
 
 import de.st_ddt.crazyloginfilter.CrazyLoginFilter;
 import de.st_ddt.crazyloginfilter.data.PlayerAccessFilter;
-import de.st_ddt.crazyplugin.commands.CrazyPlayerDataCommandExecutor;
+import de.st_ddt.crazyplugin.commands.CrazyPlayerDataPluginCommandExecutor;
 
-public abstract class CrazyLoginFilterCommandExecutor extends CrazyPlayerDataCommandExecutor<PlayerAccessFilter, CrazyLoginFilter>
+public abstract class CrazyLoginFilterCommandExecutor extends CrazyPlayerDataPluginCommandExecutor<PlayerAccessFilter, CrazyLoginFilter>
 {
 
 	public CrazyLoginFilterCommandExecutor(final CrazyLoginFilter plugin)
