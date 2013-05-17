17c95b5add96dd29b6719ac63d7a7b9624414227
ST-DDT <ST-DDT@gmx.de>
2013-05-17 22:50:45 +0200

    CrazyOnline: updated to latest CrazyCore version

--- CrazyOnline/src/de/st_ddt/crazyonline/commands/CrazyOnlineCommandExecutor.java
+++ CrazyOnline/src/de/st_ddt/crazyonline/commands/CrazyOnlineCommandExecutor.java
@@ -2,9 +2,9 @@ package de.st_ddt.crazyonline.commands;
 
 import de.st_ddt.crazyonline.CrazyOnline;
 import de.st_ddt.crazyonline.data.OnlineData;
-import de.st_ddt.crazyplugin.commands.CrazyPlayerDataCommandExecutor;
+import de.st_ddt.crazyplugin.commands.CrazyPlayerDataPluginCommandExecutor;
 
-public abstract class CrazyOnlineCommandExecutor extends CrazyPlayerDataCommandExecutor<OnlineData, CrazyOnline>
+public abstract class CrazyOnlineCommandExecutor extends CrazyPlayerDataPluginCommandExecutor<OnlineData, CrazyOnline>
 {
 
 	public CrazyOnlineCommandExecutor(final CrazyOnline plugin)
