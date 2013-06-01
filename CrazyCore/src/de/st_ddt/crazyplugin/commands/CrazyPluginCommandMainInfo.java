fd1699e9548fa5ea07d0e05372c27b4106619dc4
ST-DDT <ST-DDT@gmx.de>
2013-06-01 14:19:07 +0200

    CrazyCore: added permission to plugin info command

--- CrazyCore/src/de/st_ddt/crazyplugin/commands/CrazyPluginCommandMainInfo.java
+++ CrazyCore/src/de/st_ddt/crazyplugin/commands/CrazyPluginCommandMainInfo.java
@@ -4,6 +4,8 @@ import org.bukkit.command.CommandSender;
 
 import de.st_ddt.crazyplugin.CrazyPluginInterface;
 import de.st_ddt.crazyplugin.exceptions.CrazyException;
+import de.st_ddt.crazyutil.modules.permissions.PermissionModule;
+import de.st_ddt.crazyutil.source.Permission;
 
 public class CrazyPluginCommandMainInfo extends CrazyPluginCommandExecutor<CrazyPluginInterface>
 {
@@ -17,5 +19,12 @@ public class CrazyPluginCommandMainInfo extends CrazyPluginCommandExecutor<Crazy
 	public void command(final CommandSender sender, final String[] args) throws CrazyException
 	{
 		plugin.show(sender, plugin.getChatHeader(), true);
+	}
+
+	@Override
+	@Permission("$CRAZYPLUGIN$.info")
+	public boolean hasAccessPermission(CommandSender sender)
+	{
+		return PermissionModule.hasPermission(sender, plugin.getName().toLowerCase() + ".info");
 	}
 }
