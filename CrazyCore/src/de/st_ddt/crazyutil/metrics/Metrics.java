479d4991b58fe3c95ffab3bedce7010975b35634
ST-DDT <ST-DDT@gmx.de>
2013-07-06 21:22:36 +0200

    CrazyCore: update plugin metrics

--- CrazyCore/src/de/st_ddt/crazyutil/metrics/Metrics.java
+++ CrazyCore/src/de/st_ddt/crazyutil/metrics/Metrics.java
@@ -28,10 +28,11 @@
 package de.st_ddt.crazyutil.metrics;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.Proxy;
 import java.net.URL;
@@ -44,6 +45,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Level;
+import java.util.zip.GZIPOutputStream;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -52,42 +54,25 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.scheduler.BukkitTask;
 
-/**
- * <p>
- * The metrics class obtains data about a plugin and submits statistics about it to the metrics backend.
- * </p>
- * <p>
- * Public methods provided by this class:
- * </p>
- * <code>
- * Graph createGraph(String name); <br/>
- * void addCustomData(BukkitMetrics.Plotter plotter); <br/>
- * void start(); <br/>
- * </code>
- */
 public class Metrics
 {
 
 	/**
 	 * The current revision number
 	 */
-	private final static int REVISION = 6;
+	private final static int REVISION = 7;
 	/**
 	 * The base url of the metrics domain
 	 */
-	private static final String BASE_URL = "http://mcstats.org";
+	private static final String BASE_URL = "http://report.mcstats.org";
 	/**
 	 * The url used to report a server's status
 	 */
-	private static final String REPORT_URL = "/report/%s";
-	/**
-	 * The separator to use for custom data. This MUST NOT change unless you are hosting your own version of metrics and want to change it.
-	 */
-	private static final String CUSTOM_DATA_SEPARATOR = "~~";
+	private static final String REPORT_URL = "/plugin/%s";
 	/**
 	 * Interval of time to ping (in minutes)
 	 */
-	private static final int PING_INTERVAL = 10;
+	private static final int PING_INTERVAL = 15;
 	/**
 	 * The plugin this metrics submits for
 	 */
@@ -96,10 +81,6 @@ public class Metrics
 	 * All of the custom graphs to submit to metrics
 	 */
 	private final Set<Graph> graphs = Collections.synchronizedSet(new HashSet<Graph>());
-	/**
-	 * The default graph, used for addCustomData when you don't want a specific graph
-	 */
-	private final Graph defaultGraph = new Graph("Default");
 	/**
 	 * The plugin configuration file
 	 */
@@ -178,22 +159,6 @@ public class Metrics
 		if (graph == null)
 			throw new IllegalArgumentException("Graph cannot be null");
 		graphs.add(graph);
-	}
-
-	/**
-	 * Adds a custom data plotter to the default graph
-	 * 
-	 * @param plotter
-	 *            The plotter to use to plot custom data
-	 */
-	public void addCustomData(final Plotter plotter)
-	{
-		if (plotter == null)
-			throw new IllegalArgumentException("Plotter cannot be null");
-		// Add the plotter to the graph o/
-		defaultGraph.addPlotter(plotter);
-		// Ensure the default graph is included in the submitted graphs
-		graphs.add(defaultGraph);
 	}
 
 	/**
@@ -362,13 +327,13 @@ public class Metrics
 		final int playersOnline = Bukkit.getServer().getOnlinePlayers().length;
 		// END server software specific section -- all code below does not use any code outside of this class / Java
 		// Construct the post data
-		final StringBuilder data = new StringBuilder();
+		final StringBuilder json = new StringBuilder(1024);
+		json.append('{');
 		// The plugin's description file containg all of the plugin data such as name, version, author, etc
-		data.append(encode("guid")).append('=').append(encode(guid));
-		encodeDataPair(data, "version", pluginVersion);
-		encodeDataPair(data, "server", serverVersion);
-		encodeDataPair(data, "players", Integer.toString(playersOnline));
-		encodeDataPair(data, "revision", String.valueOf(REVISION));
+		appendJSONPair(json, "guid", guid);
+		appendJSONPair(json, "plugin_version", pluginVersion);
+		appendJSONPair(json, "server_version", serverVersion);
+		appendJSONPair(json, "players_online", Integer.toString(playersOnline));
 		// New data as of R6
 		final String osname = System.getProperty("os.name");
 		String osarch = System.getProperty("os.arch");
@@ -378,39 +343,47 @@ public class Metrics
 		// normalize os arch .. amd64 -> x86_64
 		if (osarch.equals("amd64"))
 			osarch = "x86_64";
-		encodeDataPair(data, "osname", osname);
-		encodeDataPair(data, "osarch", osarch);
-		encodeDataPair(data, "osversion", osversion);
-		encodeDataPair(data, "cores", Integer.toString(coreCount));
-		encodeDataPair(data, "online-mode", Boolean.toString(onlineMode));
-		encodeDataPair(data, "java_version", java_version);
+		appendJSONPair(json, "osname", osname);
+		appendJSONPair(json, "osarch", osarch);
+		appendJSONPair(json, "osversion", osversion);
+		appendJSONPair(json, "cores", Integer.toString(coreCount));
+		appendJSONPair(json, "auth_mode", onlineMode ? "1" : "0");
+		appendJSONPair(json, "java_version", java_version);
 		// If we're pinging, append it
 		if (isPing)
-			encodeDataPair(data, "ping", "true");
-		// Acquire a lock on the graphs, which lets us make the assumption we also lock everything
-		// inside of the graph (e.g plotters)
-		synchronized (graphs)
-		{
-			final Iterator<Graph> iter = graphs.iterator();
-			while (iter.hasNext())
+			appendJSONPair(json, "ping", "1");
+		if (graphs.size() > 0)
+			synchronized (graphs)
 			{
-				final Graph graph = iter.next();
-				for (final Plotter plotter : graph.getPlotters())
+				json.append(',');
+				json.append('"');
+				json.append("graphs");
+				json.append('"');
+				json.append(':');
+				json.append('{');
+				boolean firstGraph = true;
+				final Iterator<Graph> iter = graphs.iterator();
+				while (iter.hasNext())
 				{
-					// The key name to send to the metrics server
-					// The format is C-GRAPHNAME-PLOTTERNAME where separator - is defined at the top
-					// Legacy (R4) submitters use the format Custom%s, or CustomPLOTTERNAME
-					final String key = String.format("C%s%s%s%s", CUSTOM_DATA_SEPARATOR, graph.getName(), CUSTOM_DATA_SEPARATOR, plotter.getColumnName());
-					// The value to send, which for the foreseeable future is just the string
-					// value of plotter.getValue()
-					final String value = Integer.toString(plotter.getValue());
-					// Add it to the http post data :)
-					encodeDataPair(data, key, value);
+					final Graph graph = iter.next();
+					final StringBuilder graphJson = new StringBuilder();
+					graphJson.append('{');
+					for (final Plotter plotter : graph.getPlotters())
+						appendJSONPair(graphJson, plotter.getColumnName(), Integer.toString(plotter.getValue()));
+					graphJson.append('}');
+					if (!firstGraph)
+						json.append(',');
+					json.append(escapeJSON(graph.getName()));
+					json.append(':');
+					json.append(graphJson);
+					firstGraph = false;
 				}
+				json.append('}');
 			}
-		}
+		// close json
+		json.append('}');
 		// Create the url
-		final URL url = new URL(BASE_URL + String.format(REPORT_URL, encode(pluginName)));
+		final URL url = new URL(BASE_URL + String.format(REPORT_URL, urlEncode(pluginName)));
 		// Connect to the website
 		URLConnection connection;
 		// Mineshafter creates a socks proxy, so we can safely bypass it
@@ -419,21 +392,38 @@ public class Metrics
 			connection = url.openConnection(Proxy.NO_PROXY);
 		else
 			connection = url.openConnection();
+		final byte[] uncompressed = json.toString().getBytes();
+		final byte[] compressed = gzip(json.toString());
+		// Headers
+		connection.addRequestProperty("User-Agent", "MCStats/" + REVISION);
+		connection.addRequestProperty("Content-Type", "application/json");
+		connection.addRequestProperty("Content-Encoding", "gzip");
+		connection.addRequestProperty("Content-Length", Integer.toString(compressed.length));
+		connection.addRequestProperty("Accept", "application/json");
+		connection.addRequestProperty("Connection", "close");
 		connection.setDoOutput(true);
+		if (debug)
+			System.out.println("[Metrics] Prepared request for " + pluginName + " uncompressed=" + uncompressed.length + " compressed=" + compressed.length);
 		// Write the data
-		final OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-		writer.write(data.toString());
-		writer.flush();
+		final OutputStream os = connection.getOutputStream();
+		os.write(compressed);
+		os.flush();
 		// Now read the response
 		final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		final String response = reader.readLine();
+		String response = reader.readLine();
 		// close resources
-		writer.close();
+		os.close();
 		reader.close();
-		if (response == null || response.startsWith("ERR"))
-			throw new IOException(response); // Throw the exception
+		if (response == null || response.startsWith("ERR") || response.startsWith("7"))
+		{
+			if (response == null)
+				response = "null";
+			else if (response.startsWith("7"))
+				response = response.substring(response.startsWith("7,") ? 2 : 1);
+			throw new IOException(response);
+		}
 		else // Is this the first update this hour?
-		if (response.contains("OK This is your first update this hour"))
+		if (response.equals("1") || response.contains("This is your first update this hour"))
 			synchronized (graphs)
 			{
 				final Iterator<Graph> iter = graphs.iterator();
@@ -444,6 +434,38 @@ public class Metrics
 						plotter.reset();
 				}
 			}
+	}
+
+	/**
+	 * GZip compress a string of bytes
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static byte[] gzip(final String input)
+	{
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		GZIPOutputStream gzos = null;
+		try
+		{
+			gzos = new GZIPOutputStream(baos);
+			gzos.write(input.getBytes("UTF-8"));
+		}
+		catch (final IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (gzos != null)
+				try
+				{
+					gzos.close();
+				}
+				catch (final IOException ignore)
+				{}
+		}
+		return baos.toByteArray();
 	}
 
 	/**
@@ -465,25 +487,83 @@ public class Metrics
 	}
 
 	/**
-	 * <p>
-	 * Encode a key/value data pair to be used in a HTTP post request. This INCLUDES a & so the first key/value pair MUST be included manually, e.g:
-	 * </p>
-	 * <code>
-	 * StringBuffer data = new StringBuffer();
-	 * data.append(encode("guid")).append('=').append(encode(guid));
-	 * encodeDataPair(data, "version", description.getVersion());
-	 * </code>
+	 * Appends a json encoded key/value pair to the given string builder.
 	 * 
-	 * @param buffer
-	 *            the stringbuilder to append the data pair onto
+	 * @param json
 	 * @param key
-	 *            the key value
 	 * @param value
-	 *            the value
+	 * @throws UnsupportedEncodingException
 	 */
-	private static void encodeDataPair(final StringBuilder buffer, final String key, final String value) throws UnsupportedEncodingException
+	private static void appendJSONPair(final StringBuilder json, final String key, final String value) throws UnsupportedEncodingException
 	{
-		buffer.append('&').append(encode(key)).append('=').append(encode(value));
+		boolean isValueNumeric = false;
+		try
+		{
+			if (value.equals("0") || !value.endsWith("0"))
+			{
+				Double.parseDouble(value);
+				isValueNumeric = true;
+			}
+		}
+		catch (final NumberFormatException e)
+		{
+			isValueNumeric = false;
+		}
+		if (json.charAt(json.length() - 1) != '{')
+			json.append(',');
+		json.append(escapeJSON(key));
+		json.append(':');
+		if (isValueNumeric)
+			json.append(value);
+		else
+			json.append(escapeJSON(value));
+	}
+
+	/**
+	 * Escape a string to create a valid JSON string
+	 * 
+	 * @param text
+	 * @return
+	 */
+	private static String escapeJSON(final String text)
+	{
+		final StringBuilder builder = new StringBuilder();
+		builder.append('"');
+		for (int index = 0; index < text.length(); index++)
+		{
+			final char chr = text.charAt(index);
+			switch (chr)
+			{
+				case '"':
+				case '\\':
+					builder.append('\\');
+					builder.append(chr);
+					break;
+				case '\b':
+					builder.append("\\b");
+					break;
+				case '\t':
+					builder.append("\\t");
+					break;
+				case '\n':
+					builder.append("\\n");
+					break;
+				case '\r':
+					builder.append("\\r");
+					break;
+				default:
+					if (chr < ' ')
+					{
+						final String t = "000" + Integer.toHexString(chr);
+						builder.append("\\u" + t.substring(t.length() - 4));
+					}
+					else
+						builder.append(chr);
+					break;
+			}
+		}
+		builder.append('"');
+		return builder.toString();
 	}
 
 	/**
@@ -493,7 +573,7 @@ public class Metrics
 	 *            the text to encode
 	 * @return the encoded text, as UTF-8
 	 */
-	private static String encode(final String text) throws UnsupportedEncodingException
+	private static String urlEncode(final String text) throws UnsupportedEncodingException
 	{
 		return URLEncoder.encode(text, "UTF-8");
 	}
