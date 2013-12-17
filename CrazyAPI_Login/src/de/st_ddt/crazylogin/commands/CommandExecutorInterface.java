3a6270ce91ef7af5c0f25fbc5c0f9c4fac6a6f82
ST-DDT <ST-DDT@gmx.de>
2013-12-17 19:57:54 +0100

    CrazyAPI_Login: moved to another repository

--- CrazyAPI_Login/src/de/st_ddt/crazylogin/commands/CommandExecutorInterface.java
+++
@@ -1,9 +0,0 @@
-package de.st_ddt.crazylogin.commands;
-
-import de.st_ddt.crazylogin.LoginPlugin;
-import de.st_ddt.crazylogin.data.LoginData;
-import de.st_ddt.crazyplugin.commands.CrazyPlayerDataPluginCommandExecutorInterface;
-
-public interface CommandExecutorInterface<S extends LoginPlugin<? extends LoginData>> extends CrazyPlayerDataPluginCommandExecutorInterface<LoginData, S>
-{
-}
